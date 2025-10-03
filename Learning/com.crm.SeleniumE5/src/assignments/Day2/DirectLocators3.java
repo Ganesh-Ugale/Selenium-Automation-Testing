7d2524e5ab70d30101c00b5c83405b028344e7d7
Ganesh-Ugale <ugale.ganesh.d@gmail.com>
2025-10-03 12:00:27 +0530

    Create DirectLocators3.java

---
+++ Learning/com.crm.SeleniumE5/src/assignments/Day2/DirectLocators3.java
@@ -0,0 +1,25 @@
+package assignments.Day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Write an Script for Demo Webshop WebApplication:
+ * 1. Open the Browser
+ * 2. After Opening it Maximize it
+ * 3. Enter into the DemowebShop website
+ * 4. Click on Shopping Cart Link
+ * 5. Close the Browser.
+ * */
+public class DirectLocators3 {
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
+		driver.findElement(By.className("ico-cart")).click();
+		Thread.sleep(3000);
+		driver.close();
+	}
+}
